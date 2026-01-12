5deb6f1ebc7418eeae76ae9bae541e9fc0173b15
danitalmaci <danitalmaci@gmail.com>
2026-01-12 17:43:50 +0100

    infoPokmon

--- app/controller/model/gestor_Pokemon.py
+++ app/controller/model/gestor_Pokemon.py
@@ -8,10 +8,18 @@ class gestorPokemon:
         rows = self.db.select(query)
         return [dict(row) for row in rows]
     
-    def mostrarPokemon(self):
-        query = "SELECT p.nombrePokemon, p.imagen,p.nombreCategoria p.altura, p.peso, p.pokedexID, p.sexo,"
-        "p.ps, p.velocidad, p.ataque, p.ataqueEspecial, p.defensa, p.defensaEspecial, h.nombreHabilidad,"
-        "t.imagenTipo FROM PokemonPokedex p INNER JOIN Posee h ON p.nombrePokemon = h.nombrePokemon "
-        "INNER JOIN Contiene t ON p.nombrePokemon = t.nombrePokemon WHERE p.nombrePokemon = Pokemon"
-        rows = self.db.select(query)
+    def mostrarPokemon(self,nombrePokemon):
+        query = (
+        "SELECT p.nombrePokemon, p.imagen, p.nombreCategoria, p.altura, p.peso, "
+        "p.pokedexID, p.sexo, p.ps, p.velocidad, p.ataque, p.ataqueEspecial, "
+        "p.defensa, p.defensaEspecial, "
+        "h.nombreHabilidad, "
+        "t.nombreTipo, tp.imagenTipo "
+        "FROM PokemonPokedex p "
+        "INNER JOIN Posee h ON p.nombrePokemon = h.nombrePokemon "
+        "INNER JOIN Contiene t ON p.nombrePokemon = t.nombrePokemon "
+        "INNER JOIN TipoPokemon tp ON t.nombreTipo = tp.nombreTipo "
+        "WHERE p.nombrePokemon = ?"
+        )
+        rows = self.db.select(query, (nombrePokemon,))
         return [dict(row) for row in rows]
